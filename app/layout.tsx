3e518fc97b89989b52a57f30e5e5ecac16c6f919
igor rypniewski <94071547+real-kijmoshi@users.noreply.github.com>
2024-11-13 21:54:05 +0100

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body>
         {children}
       </body>
